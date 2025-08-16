a7c485292f9e7186f75748b0703a71be64512b2d
Soham908 <sohampalekar908@gmail.com>
2025-08-16 19:43:26 +0530

    added module file

---
+++ src/nexrender.d.ts
@@ -0,0 +1,5 @@
+declare module './nexrender.js' {
+  export function createNexrenderJob(...args: any[]): any;
+  export function getJobStatus(...args: any[]): any;
+  export function buildJobPayloadFromForm(...args: any[]): any;
+}
